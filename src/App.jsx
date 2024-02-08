e3b8a3e3186f2d541dca5dda9a0f3d7ebaec54d2
leader-akash <leader.akashak@gmail.com>
2024-02-08 17:18:18 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import Home from './pages/home/Home'
 import Room from './pages/room/Room'
 
 function App() {
-
+  
   return (
-    <div className='App'>
-      <h1>Video call</h1>
-      <Router>
-        <Routes>
+   <div className='App'>
+   <h1>Video call</h1>
+      <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/room/:roomId' element={<Room />} />
-        </Routes>
-      </Router>
-    </div>
+      </Routes> 
+   </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { SocketProvider } from './providers/Socket.jsx'
-import { PeerProvider } from './providers/Peer.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { SocketProvider } from './providers/Socket.jsx';
+import { PeerProvider } from './providers/Peer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <SocketProvider>
-        <PeerProvider>
+    <SocketProvider>
+      <PeerProvider>
+        <BrowserRouter>
           <App />
-        </PeerProvider>
-      </SocketProvider>
-    </BrowserRouter>
-  </React.StrictMode >,
+        </BrowserRouter>
+      </PeerProvider>
+    </SocketProvider>
+  </React.StrictMode> 
 )
